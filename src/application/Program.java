95ce479874eb45cdbd1da4278d60130d968fb6cd
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-12 11:46:48 +0100

    insert data in dep...

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -20,21 +22,37 @@ public class Program {
             conn = DB.getConnection();
             
             
-            st = conn.prepareStatement(
+           /* st = conn.prepareStatement(
                 "INSERT INTO seller " +
                 "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +  
                 "VALUES " +
                 "(?, ?, ?, ?, ?)"  
-            );
+            , Statement.RETURN_GENERATED_KEYS);
             
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setString(1, "Lucian Alfred");
+            st.setString(2, "la@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("11/11/2001").getTime()));
             st.setDouble(4, 3000.0);
             st.setInt(5, 4);
+            */
+            st = conn.prepareStatement(
+            		"insert into department (Name) values ('D1'), ('D2)", Statement.RETURN_GENERATED_KEYS
+            		);
+            
             
             int rowsAffected = st.executeUpdate();
-            System.out.println("Done! Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+            	ResultSet rs = st.getGeneratedKeys();
+            	while (rs.next()) {
+            		int id = rs.getInt(1);
+            		System.out.print("Done! Id = "+id+"\n");
+            		
+            	}
+            	System.out.println("Done! Rows affected: " + rowsAffected);
+            }else {
+            	System.out.println("Done! No Rows affected: ");
+            }
+            
             
         } catch (SQLException e) {
             System.err.println("SQL Error: " + e.getMessage());
